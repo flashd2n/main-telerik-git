0912163cda54c12149b2cd50fa009cd39de982f3
flash <k.kostov.eu@gmail.com>
2016-12-16 21:37:13 +0200

    m

---
+++ c-sharp-advanced/Training/methods/exercises/methodsExs/test2/test2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string temp = Console.ReadLine();
+
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] == '+')
+                {
+                    temp = temp.Insert(i, " ");
+                    temp = temp.Insert(i + 2, " ");
+                    i += 3;
+                }
+            }
+            Console.WriteLine(temp);
+
+        }
+    }
+}
