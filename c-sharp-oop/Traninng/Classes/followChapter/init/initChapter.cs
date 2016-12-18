b4d3382e838866a79b6f1a4a3f7113e7392a0967
flash <k.kostov.eu@gmail.com>
2016-12-18 22:14:08 +0200

    start classes chapter

--- c-sharp-oop/Traninng/Classes/followChapter/init/initChapter.cs
+++ c-sharp-oop/Traninng/Classes/followChapter/init/initChapter.cs
@@ -10,9 +10,63 @@ namespace init
     {
         static void Main()
         {
+            string firstDogName = null;
+            Console.WriteLine("Enter first dog name:");
+            firstDogName = Console.ReadLine();
 
+            var firstDog = new Dog(firstDogName);
 
+            var secondDog = new Dog();
 
+            Console.WriteLine("Enter second dog name:");
+            secondDog.Name = Console.ReadLine();
+
+            var thirdDog = new Dog();
+
+            Dog[] dogs = new Dog[] { firstDog, secondDog, thirdDog};
+
+            foreach (var dog in dogs)
+            {
+                dog.Bark();
+            }
+
+        }
+    }
+
+    public class Dog
+    {
+        //Field declaration
+        private string name;
+        //Constructor declaration
+        public Dog()
+        {
+
+        }
+
+        //Another constructor declaration
+        public Dog(string name)
+        {
+            this.name = name;
+        }
+
+        //Property declaration
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        //Method Declaration
+        public void Bark()
+        {
+            Console.WriteLine($"{name ?? "[unnamed dog]"} said: wof wof!");
         }
     }
 }
