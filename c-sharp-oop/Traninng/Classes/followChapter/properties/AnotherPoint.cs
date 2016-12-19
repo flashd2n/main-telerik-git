4b477d9a0403be866ee590b08feae96c53d0f24c
flash <k.kostov.eu@gmail.com>
2016-12-19 19:15:12 +0200

    switch office

---
+++ c-sharp-oop/Traninng/Classes/followChapter/properties/AnotherPoint.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties
+{
+    class AnotherPoint
+    {
+
+        private double[] coordinates;
+
+        public AnotherPoint(double x, double y)
+        {
+            this.coordinates = new double[2];
+
+            this.coordinates[0] = x;
+            this.coordinates[1] = y;
+        }
+
+        public double X
+        {
+            get { return coordinates[0]; }
+            set { coordinates[0] = value; }
+        }
+
+        public double Y
+        {
+            get { return coordinates[1]; }
+            set { coordinates[1] = value; }
+        }
+    }
+}
---
+++ c-sharp-oop/Traninng/Classes/followChapter/properties/Properties.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties
+{
+    class Properties
+    {
+        static void Main()
+        {
+            var pointOne = new Point(1.2, 1.5);
+            Console.WriteLine(pointOne.X);
+            Console.WriteLine(pointOne.Y);
+            var pointTwo = new AnotherPoint(3.4, 5.1);
+            Console.WriteLine(pointTwo.X);
+            Console.WriteLine(pointTwo.Y);
+        }
+    }
+
+    public class Point
+    {
+        private double x;
+        private double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double X
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                this.x = value;
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                this.y = value;
+            }
+        }
+    }
+}
