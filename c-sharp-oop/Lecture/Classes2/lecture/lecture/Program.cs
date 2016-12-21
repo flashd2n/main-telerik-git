9bd79a3314b07330b6b7eba68fffa098d7b5c8be
flash <k.kostov.eu@gmail.com>
2016-12-21 21:50:33 +0200

    end

---
+++ c-sharp-oop/Lecture/Classes2/lecture/lecture/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = 1;
+
+            Console.WriteLine("Number: {0}", a);
+            Console.WriteLine($"Number: {a}");
+
+
+
+        }
+    }
+}
