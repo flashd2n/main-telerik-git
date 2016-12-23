3b37a165ad90cf8594b2268c672e59509ddbea24
flash <k.kostov.eu@gmail.com>
2016-12-23 12:34:11 +0200

    switch

--- c-sharp-oop/Traninng/Classes/followChapter/InnerClasses/Program.cs
+++ c-sharp-oop/Traninng/Classes/followChapter/InnerClasses/Program.cs
@@ -8,9 +8,41 @@ namespace InnerClasses
 {
     class Program
     {
-        static void Main()
+        private string name;
+
+        private Program(string name)
         {
+            this.name = name;
+        }
+
+        private class NestedClass
+        {
+            private string name;
+            private Program parent;
+
+            public NestedClass(Program parent, string name)
+            {
+                this.parent = parent;
+                this.name = name;
+            }
+
+            public void PrintNames()
+            {
+                Console.WriteLine($"Nested name: {this.name}");
+                Console.WriteLine($"Outer name: {this.parent.name}");
+            }
 
         }
+
+        static void Main()
+        {
+            var outerClass = new Program("Kalin");
+            var nestedClass = new Program.NestedClass(outerClass, "Gosho");
+            nestedClass.PrintNames();
+
+
+        }
+
+
     }
 }
