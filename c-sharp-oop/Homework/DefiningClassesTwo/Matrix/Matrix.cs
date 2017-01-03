eb0f2d4c91936c4c68077f22560674cfadbac89a
flash <k.kostov.eu@gmail.com>
2017-01-04 01:40:41 +0200

    almost done classes two hw

--- c-sharp-oop/Homework/DefiningClassesTwo/Matrix/Matrix.cs
+++ c-sharp-oop/Homework/DefiningClassesTwo/Matrix/Matrix.cs
@@ -1,24 +1,151 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Matrix
 {
     class Matrix<T>
     {
+        private T[,] matrix;
         private int row;
         private int col;
-        private T[,] matrix;
 
-        public int Row
+        public Matrix(int row, int col)
+        {
+            this.matrix = new T[row, col];
+            this.Row = row;
+            this.Col = col;
+        }
+
+        public T this[int row, int col]
+        {
+            get { return this.matrix[row, col]; }
+            set { this.matrix[row, col] = value; }
+        }
+
+        private int Row
         {
             get { return this.row; }
             set { this.row = value; }
         }
 
+        private int Col
+        {
+            get { return this.col; }
+            set { this.col = value; }
+        }
 
+        public static Matrix<T> operator +(Matrix<T> matrixOne, Matrix<T> matrixTwo)
+        {
+            var result = new Matrix<T>(matrixOne.Row, matrixTwo.Col);
 
+            if (matrixOne.Row != matrixTwo.Row || matrixOne.Col != matrixTwo.Col)
+            {
+                throw new Exception("The matrices are not the same size!");
+            }
+            else
+            {
+                for (int i = 0; i < matrixOne.Row; i++)
+                {
+                    for (int j = 0; j < matrixOne.Col; j++)
+                    {
+                        result[i, j] = (dynamic)matrixOne[i, j] + matrixTwo[i, j];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix<T> operator -(Matrix<T> matrixOne, Matrix<T> matrixTwo)
+        {
+            var result = new Matrix<T>(matrixOne.Row, matrixTwo.Col);
+
+            if (matrixOne.Row != matrixTwo.Row || matrixOne.Col != matrixTwo.Col)
+            {
+                throw new Exception("The matrices are not the same size!");
+            }
+            else
+            {
+                for (int i = 0; i < matrixOne.Row; i++)
+                {
+                    for (int j = 0; j < matrixOne.Col; j++)
+                    {
+                        result[i, j] = (dynamic)matrixOne[i, j] - matrixTwo[i, j];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix<T> operator *(Matrix<T> matrixOne, Matrix<T> matrixTwo)
+        {
+            var result = new Matrix<T>(matrixOne.Row, matrixTwo.Col);
+
+            if (matrixOne.Row != matrixTwo.Row || matrixOne.Col != matrixTwo.Col)
+            {
+                throw new Exception("The matrices are not the same size!");
+            }
+            else
+            {
+                for (int i = 0; i < matrixOne.Row; i++)
+                {
+                    for (int j = 0; j < matrixOne.Col; j++)
+                    {
+                        result[i, j] = (dynamic)matrixOne[i, j] * matrixTwo[i, j];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static bool operator true(Matrix<T> matrixToCheck)
+        {
+            bool result = false;
+
+            for (int i = 0; i < matrixToCheck.Row; i++)
+            {
+                for (int j = 0; j < matrixToCheck.Col; j++)
+                {
+                    if ((dynamic)matrixToCheck[i,j] == 0)
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static bool operator false(Matrix<T> matrixToCheck)
+        {
+            bool result = false;
+
+            for (int i = 0; i < matrixToCheck.Row; i++)
+            {
+                for (int j = 0; j < matrixToCheck.Col; j++)
+                {
+                    if ((dynamic)matrixToCheck[i, j] == 0)
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < this.Row; i++)
+            {
+                for (int j = 0; j < this.Col; j++)
+                {
+                    Console.Write("{0}{1}", matrix[i, j], j == (matrix.GetLength(1) - 1) ? "\n" : " ");
+                }
+            }
+        }
     }
 }
---
+++ c-sharp-oop/Homework/DefiningClassesTwo/Matrix/Startup.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var myMatrix = new Matrix<int>(3, 3);
+
+            myMatrix[0, 0] = 1;
+            myMatrix[0, 1] = 2;
+            myMatrix[0, 2] = 3;
+
+            myMatrix[1, 0] = 4;
+            myMatrix[1, 1] = 5;
+            myMatrix[1, 2] = 6;
+
+            myMatrix[2, 0] = 7;
+            myMatrix[2, 1] = 8;
+            myMatrix[2, 2] = 9;
+
+            Console.WriteLine(myMatrix[2,2]);
+
+            Console.WriteLine("=====");
+
+            myMatrix.Print();
+
+            var myMatrixTwo = new Matrix<int>(3, 3);
+
+            myMatrixTwo[0, 0] = 1;
+            myMatrixTwo[0, 1] = 2;
+            myMatrixTwo[0, 2] = 3;
+
+            myMatrixTwo[1, 0] = 4;
+            myMatrixTwo[1, 1] = 5;
+            myMatrixTwo[1, 2] = 6;
+
+            myMatrixTwo[2, 0] = 7;
+            myMatrixTwo[2, 1] = 8;
+            myMatrixTwo[2, 2] = 9;
+
+            var additionMatrix = myMatrix + myMatrixTwo;
+
+            Console.WriteLine("=====");
+
+            additionMatrix.Print();
+
+            Console.WriteLine("======");
+
+            var subtractionMatrix = myMatrix - myMatrixTwo;
+
+            subtractionMatrix.Print();
+
+            Console.WriteLine("=======");
+
+            var productMatrix = myMatrix * myMatrixTwo;
+
+            productMatrix.Print();
+
+            if (myMatrix)
+            {
+                Console.WriteLine("There is a zero cell");
+            }
+            else
+            {
+                Console.WriteLine("There is NO zero cell");
+            }
+
+            var myMatrixThree = new Matrix<int>(3, 3);
+
+            myMatrixThree[0, 0] = 1;
+            myMatrixThree[0, 1] = 2;
+            myMatrixThree[0, 2] = 3;
+
+            myMatrixThree[1, 0] = 4;
+            myMatrixThree[1, 1] = 0;
+            myMatrixThree[1, 2] = 6;
+
+            myMatrixThree[2, 0] = 7;
+            myMatrixThree[2, 1] = 8;
+            myMatrixThree[2, 2] = 9;
+
+            Console.WriteLine("========");
+
+            if (myMatrixThree)
+            {
+                Console.WriteLine("There is a zero cell");
+            }
+            else
+            {
+                Console.WriteLine("There is NO zero cell");
+            }
+        }
+    }
+}
