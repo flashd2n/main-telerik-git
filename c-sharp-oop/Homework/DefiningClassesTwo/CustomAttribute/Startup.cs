3aa71220a16ed2cbe758cad9bee9ed8f0e6ec1c4
flash <k.kostov.eu@gmail.com>
2017-01-04 02:40:38 +0200

    classes two hw complete

---
+++ c-sharp-oop/Homework/DefiningClassesTwo/CustomAttribute/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace CustomAttribute
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var sampleTest = new Sample();
+
+            Console.WriteLine(sampleTest.GetType().GetCustomAttribute<Version>().FullVersion);
+        }
+    }
+
+    [Version(2.11)]
+    public class Sample
+    {
+
+    }
+}
---
+++ c-sharp-oop/Homework/DefiningClassesTwo/CustomAttribute/Version.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CustomAttribute
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface | AttributeTargets.Enum | AttributeTargets.Method)]
+    public class Version : Attribute
+    {
+        private double fullVersion;
+
+        public Version(double version)
+        {
+            this.FullVersion = version;
+        }
+
+        public double FullVersion
+        {
+            get { return this.fullVersion; }
+            private set { this.fullVersion = value; }
+        }
+    }
+}
