18f9caef2c302436d15c1c3117beca81d3fb724f
flash <k.kostov.eu@gmail.com>
2017-01-11 11:47:45 +0200

    ood hw two task three complete

---
+++ c-sharp-oop/Homework/OOPtwo/OOPhwTwo/BankAccounts/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ c-sharp-oop/Homework/OOPtwo/OOPhwTwo/RangeExceptions/InvalidRangeException.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeExceptions
+{
+    public class InvalidRangeException<T> : ApplicationException
+    {
+        // Fields
+        private T start;
+        private T end;
+
+        // Constructors
+        public InvalidRangeException(string message, T start, T end, Exception causeException)
+            : base(message, causeException)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public InvalidRangeException(string message, T start, T end)
+            : this(message, start, end, null)
+        {
+
+        }
+
+        // Properties
+        public T Start
+        {
+            get { return this.start; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Start range cannot be null!");
+                }
+                else
+                {
+                    this.start = value;
+                }
+            }
+        }
+
+        public T End
+        {
+            get { return this.end; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("End range cannot be null!");
+                }
+                else
+                {
+                    this.end = value;
+                }
+            }
+        }
+    }
+}
