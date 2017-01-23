1f67b69b028ebdb985289a82d93692254a2c5228
flash <k.kostov.eu@gmail.com>
2017-01-23 16:40:38 +0200

    switch fera

---
+++ c-sharp-DSA-prep/Training/RecursionBook/Factorial/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Startup
+    {
+        static void Main()
+        {
+            int n = 5;
+            int factorialN = Fact(n);
+
+            Console.WriteLine(factorialN);
+        }
+
+        static int Fact(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Fact(n - 1);
+            }
+        }
+    }
+}
