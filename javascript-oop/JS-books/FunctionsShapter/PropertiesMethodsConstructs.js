d1770d7e2ee870598dbc3d7032de0f67b9906cdd
flash <k.kostov.eu@gmail.com>
2017-02-01 17:30:38 +0200

    .

--- javascript-oop/JS-books/FunctionsShapter/PropertiesMethodsConstructs.js
+++ javascript-oop/JS-books/FunctionsShapter/PropertiesMethodsConstructs.js
@@ -1,2 +1,17 @@
-'use strict';
 
+
+function funcOne(args){
+    let actual = args.length; // the number of oreceived parameters
+    console.log(actual);
+    let expected = args.callee.length; // the number of parameters defined in the calling function
+    console.log(expected);
+}
+
+function mainFunction(x, y, z){
+    funcOne(arguments);
+    return (x + y + z);
+}
+
+
+mainFunction(1, 2, 3, 4);
+console.log(mainFunction.length); // the number of parameters defined in the function definition
