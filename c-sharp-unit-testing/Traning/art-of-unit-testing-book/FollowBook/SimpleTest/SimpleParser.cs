c283ca0bea46ed0468b9e898eb172448bf147905
flash <k.kostov.eu@gmail.com>
2017-02-03 17:02:16 +0200

    switch telerik

---
+++ c-sharp-unit-testing/Traning/art-of-unit-testing-book/FollowBook/SimpleTest/SimpleParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTest
+{
+    public static class SimpleParser
+    {
+        public static int ParseAndSum(string numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return 0;
+            } else if (!numbers.Contains(","))
+            {
+                return int.Parse(numbers);
+            }
+            else
+            {
+                throw new ArgumentException("I can only handle 0 or 1 numbers");
+            }
+        }
+    }
+}
---
+++ c-sharp-unit-testing/Traning/art-of-unit-testing-book/FollowBook/SimpleTest/SimpleParserTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTest
+{
+    public static class SimpleParserTest
+    {
+        public static void TestReturnsZeroWhenEmptyString()
+        {
+
+            try
+            {
+                int result = SimpleParser.ParseAndSum(string.Empty);
+                if (result != 0)
+                {
+                    Console.WriteLine("SimpleParser class should have returned 0 when passed an empty string");
+                }
+                if (result == 0)
+                {
+                    Console.WriteLine("Test passed");
+                }
+
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+        }
+
+
+    }
+}
