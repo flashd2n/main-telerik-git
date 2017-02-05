00dce8296b93acb90cbd7c0cd988df0e4352d608
flash <k.kostov.eu@gmail.com>
2017-02-05 19:58:13 +0200

    fffffffff javascript

--- javascript-oop/JS-books/testing.js
+++ javascript-oop/JS-books/testing.js
@@ -1,26 +1,15 @@
 'use strict';
 
-let point = {
-    x: 1,
-    y: 2
-};
+function solve() {
 
-if (`x` in point) {
-    console.log(`Yes, there is x in point`);
-} else{
-    console.log(`No, there isn't x in point`);
+	let array = [].slice.call(arguments);
+
+	return function sum() {
+
+		let result = array.reduce((x,y) => x + y);
+		return result;
+
+	};
 }
 
-
-let myArray = [1,2,3,4,5];
-delete myArray[2];
-console.log(myArray);
-
-let myObject = {
-    x: 1,
-    y: 2,
-    z: 3
-};
-
-delete myObject.a;
-console.log(myObject);
+console.log(solve([1, 2, 3]));
