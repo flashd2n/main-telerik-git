2f82e721813b1cd111a3c434c36986f0c4309052
flash <k.kostov.eu@gmail.com>
2017-02-08 16:41:31 +0200

    progress art of unit testing examples

--- c-sharp-unit-testing/Training/art-of-unit-testing-book/MainProject/LogAn.UnitTests/LogAnalyzerTests.cs
+++ c-sharp-unit-testing/Training/art-of-unit-testing-book/MainProject/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using MainProject;
+using System;
 
 namespace LogAn.UnitTests
 {
@@ -20,9 +21,9 @@ namespace LogAn.UnitTests
 
         }
 
-        [TestCase("Goodextensionnameuppercase.SLF")]
-        [TestCase("Goodextensionnamelowercase.slf")]
-        public void IsValidLogFileNAme_GoodExtension_ReturnsTrue(string fileName)
+        [TestCase("Goodextensionnameuppercase.SLF", true)]
+        [TestCase("Goodextensionnamelowercase.slf", true)]
+        public void IsValidLogFileNAme_GoodExtension_ReturnsTrue(string fileName, bool expected)
         {
             // Arrange
 
@@ -34,10 +35,74 @@ namespace LogAn.UnitTests
 
             // Assert
 
-            Assert.True(result);
+            Assert.AreEqual(result, expected);
 
         }
 
+        [TestCase("filewithbadextension.foo", false)]
+        public void IsValidLogFileName_BadExtension_ReturnsFalse(string fileName, bool expected)
+        {
+            // Arrange
 
+            var sut = new LogAnalyzer();
+
+            // Act
+
+            var result = sut.IsValidLogFileName(fileName);
+
+            //Assert
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [Test]
+        public void IsValidLogFileName_EmptyString_Throws()
+        {
+            // Arrange
+
+            var sut = new LogAnalyzer();
+            
+            // Act & Assert
+
+            var ex = Assert.Catch<ArgumentException>(() => sut.IsValidLogFileName(""));
+            StringAssert.Contains("Filename cannot be empty.", ex.Message);
+        }
+
+        [Test]
+        [Ignore("There is a problem with this test")]
+        public void Some_IrrelevantTest_ThatShouldBeIgnored()
+        {
+
+        }
+
+        [Test]
+        public void IsValidFileName_WhenCalledBadValue_ChangesWasLastFileNameValidToFalse()
+        {
+            // Arrange
+            var sut = new LogAnalyzer();
+
+            // Act
+
+            sut.IsValidLogFileName("badfilename.foo");
+
+            // Assert
+
+            Assert.False(sut.WasLastFileNameValid);
+        }
+
+        [Test]
+        public void IsValidFileName_WhenCalledGoodValue_ChangesWasLastFileNameValidToTrue()
+        {
+            // Arrange
+            var sut = new LogAnalyzer();
+
+            // Act
+
+            sut.IsValidLogFileName("goodfilename.slf");
+
+            // Assert
+
+            Assert.True(sut.WasLastFileNameValid);
+        }
     }
 }
---
+++ c-sharp-unit-testing/Training/art-of-unit-testing-book/SimpleCalculator/MemCalculator/CalculatorEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemCalculator
+{
+    public class CalculatorEngine
+    {
+        private int sum;
+
+        public CalculatorEngine()
+        {
+            this.sum = 0;
+        }
+
+        public void Add(int number)
+        {
+            sum += number;
+        }
+
+        public int Equals()
+        {
+            var result = this.sum;
+            this.sum = 0;
+            return result;
+        }
+
+    }
+}
