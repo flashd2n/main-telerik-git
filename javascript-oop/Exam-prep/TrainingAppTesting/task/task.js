441e8d835a14bee8bc668cbecc76ff170f5bda2d
flash <k.kostov.eu@gmail.com>
2017-02-25 21:30:01 +0200

    set up task directory

---
+++ javascript-oop/Exam-prep/TrainingAppTesting/task/task.js
@@ -0,0 +1,10 @@
+function solve() {
+	// Your classes
+
+	return createTrainingPlanner(personalData){
+        // returns new training planner object
+    };
+}
+
+// Submit the code above this line in bgcoder.com
+module.exports = solve;
