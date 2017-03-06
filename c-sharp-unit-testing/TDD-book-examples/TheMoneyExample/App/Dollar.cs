26bf2f4b9776007a891aa2b4116148eeaf69b3f8
flash <k.kostov.eu@gmail.com>
2017-03-06 14:03:55 +0200

    init tdd-book

---
+++ c-sharp-unit-testing/TDD-book-examples/TheMoneyExample/App/Dollar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class Dollar
+    {
+        public int amount = 10;
+
+        public Dollar(int amount)
+        {
+
+        }
+
+        public void times(int multiplier)
+        {
+
+        }
+
+    }
+}
---
+++ c-sharp-unit-testing/TDD-book-examples/TheMoneyExample/AppTests/MultiCurrencyTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using App;
+
+namespace AppTests
+{
+    [TestFixture]
+    public class MultiCurrencyTests
+    {
+        [Test]
+        public void TestMultiplication()
+        {
+            var five = new Dollar(5);
+            five.times(2);
+
+            Assert.AreEqual(10, five.amount);
+        }
+            
+
+    }
+}
