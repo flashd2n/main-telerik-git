0dfe34370dbce430460885119c2b35565004d8fa
flash <k.kostov.eu@gmail.com>
2017-04-23 15:13:46 +0300

    finish ajax training one

---
+++ javascript-applications/js-and-ajax-training/Exercise Files/04-01/workingfolder/ajax/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+$.getJSON('data.json', function(data){
+
+    let allItems = $('<ul>').addClass('searchResults');
+
+    $.each(data, function(key, value){
+        
+        let item = $('<li>');
+
+        $('<h2>').html(value.name).appendTo(item);
+        $('<img>').attr('src', `/images/${value.shortname}_tn.jpg`).appendTo(item);
+        $('<p>').html(value.bio).appendTo(item);
+
+        item.appendTo(allItems);
+
+    });
+
+    $('#update').append(allItems);
+
+});
