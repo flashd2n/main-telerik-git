07425a6ad85164ad35a9a4979481255a65ec4d93
Kalin Kostov <k.kostov.eu@gmail.com>
2017-06-02 21:55:23 +0300

    entity training progress

---
+++ databases/Training/Entity-CF/EntityIntro/EFTesting/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTesting
+{
+    public class Startup
+    {
+        static void Main()
+        {
+            var dbContext = new TelerikAcademyEntities();
+
+            var list = new List<Project>()
+                    .FirstOrDefault(pr => pr.ProjectID == 1); // FUNC
+
+            var project = dbContext.Projects    
+                    .FirstOrDefault(pr => pr.ProjectID == 1); // EXPRESSION
+
+            // custom expression
+
+            Expression<Func<Project, bool>> expr = pr => pr.ProjectID == 1;
+
+            // re-writen
+
+            var list2 = new List<Project>()
+                    .FirstOrDefault(CustomFunc); // FUNC
+
+            var project2 = dbContext.Projects
+                    .FirstOrDefault(expr); // EXPRESSION
+
+            var employeesByDeparments = dbContext.Employees
+                .GroupBy(e => e.Department.Name)
+                .ToList();
+
+            foreach (var emplGroup in employeesByDeparments)
+            {
+                Console.WriteLine(emplGroup.Key);
+                foreach (var empl in emplGroup)
+                {
+                    Console.WriteLine(empl.FirstName + " " + empl.LastName);
+                }
+                Console.WriteLine("--------");
+            }
+
+        }
+
+        // custom func
+        private static bool CustomFunc(Project project)
+        {
+            return project.ProjectID == 1;
+        }
+
+    }
+}
