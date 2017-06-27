04d01a839616ddf5481eabf0410cdfa4becde7c8
flash <k.kostov.eu@gmail.com>
2017-06-27 23:01:01 +0300

    I don't even know what I did here

---
+++ data-structures-algorithms/Training/Book/Linear-data-structures/Training/Lists/ArrayList.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace Lists
+{
+    public class ArrayList<T>
+    {
+        private const int DefaultInitialCapacity = 4;
+        private const int ResizeCoeficient = 2;
+        private T[] buffer;
+        private int count;
+
+        public ArrayList(int initialCapacity = DefaultInitialCapacity)
+        {
+            this.buffer = new T[initialCapacity];
+            this.count = 0;
+        }
+
+        public int Capacity => buffer.Length;
+        public int Count => this.count;
+
+        public T this[int index]
+        {
+            get
+            {
+                return buffer[index];
+            }
+            set
+            {
+                if (index < 0 || index >= this.count)
+                {
+                    throw new ArgumentOutOfRangeException("Not cool bro");
+                }
+
+                buffer[index] = value;
+            }
+        }
+
+        public void Add(T value)
+        {
+
+            this.IncreaseBufferIfNeeded();
+
+            buffer[this.count] = value;
+            ++this.count;
+        }
+
+        public void Insert(int position, T value)
+        {
+            if (position < 0 || position > this.count)
+            {
+                throw new ArgumentOutOfRangeException("Not cool bro");
+            }
+
+            if (position == this.Count)
+            {
+                this.Add(value);
+                return;
+            }
+
+            this.IncreaseBufferIfNeeded();
+
+            for (int i = this.count; i > position; i--)
+            {
+                buffer[i] = buffer[i - 1];
+            }
+
+            buffer[position] = value;
+            ++this.count;
+
+        }
+
+        public void Clear()
+        {
+            this.buffer = new T[DefaultInitialCapacity];
+            this.count = 0;
+        }
+
+        public bool Contains(T value)
+        {
+            if (this.IndexOf(value) != -1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Remove(T value)
+        {
+            // TODO
+        }
+
+        public void RemoveAt(int position, T value)
+        {
+            // TODO
+        }
+
+        public int IndexOf(T value)
+        {
+            for (int i = 0; i < count; i++)
+            {
+
+                if (Object.Equals(this.buffer[i], value))
+                {
+                    return i;
+                }
+
+            }
+
+            return -1;
+        }
+
+        private void IncreaseBufferIfNeeded()
+        {
+            if (this.Count == this.Capacity)
+            {
+                var newCapacity = this.Capacity * ResizeCoeficient;
+
+                var newBuffer = new T[newCapacity];
+
+                for (int i = 0; i < this.buffer.Length; i++)
+                {
+                    newBuffer[i] = buffer[i];
+
+                    buffer[i] = default(T);
+
+                }
+
+                this.buffer = newBuffer;
+            }
+
+        }
+    }
+}
---
+++ data-structures-algorithms/Training/Book/Linear-data-structures/Training/Lists/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+
+            var test = new ArrayList<int>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                test.Add(i);
+            }
+
+            test.Insert(2, 42);
+
+            test[0] = 69;
+
+            for (int i = 0; i < test.Count; i++)
+            {
+                Console.WriteLine(test[i]);
+            }
+
+        }
+    }
+}
