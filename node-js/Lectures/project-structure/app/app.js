88b2beee955cfbc4edce50ca213b49df317b05c6
flash <k.kostov.eu@gmail.com>
2017-07-21 22:42:31 +0300

    rf

--- node-js/Lectures/project-structure/app/app.js
+++ node-js/Lectures/project-structure/app/app.js
@@ -1,8 +1,11 @@
+/* globals __dirname */
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const path = require('path');
+const controller = require('./controllers/all.controller');
 
 const init = (data) => {
     const app = express();
@@ -27,46 +30,19 @@ const init = (data) => {
     });
 
     app.get('/items', (req, res) => {
-        return data.items.getAll()
-            .then((items) => {
-                console.log(items[0].id);
-                return res.render('items/all', { context: items });
-            });
+        controller.getItems(req, res, data);
     });
 
-    app.post('/items', (req, res, next) => {
-        const item = req.body;
-
-        if (typeof item !== undefined &&
-            typeof item.text === 'string' &&
-            item.text.length > 3) {
-            return next();
-        }
-
-        req.flash('error', 'Invalid model');
-
-        return res.redirect('/form');
-
-    }, (req, res) => {
-        const item = req.body;
-
-        data.items.create(item)
-            .then((dbItem) => {
-                console.log(dbItem);
-                console.log(dbItem.id);
-                console.log(typeof dbItem.id);
-                return res.redirect('/items/' + dbItem.id);
-            })
-            .catch((err) => {
-                res.send('Sorry bro');
-            });
+    app.post('/items', controller.validation, (req, res) => {
+        controller.submitItem(req, res, data);
     });
 
     app.get('/form', (req, res) => {
         res.render('items/form');
     });
 
-    return Promise.resolve(app); // good idea in order unify all init operations (db connection, data init, app init and so on) to work with promises
+    return Promise.resolve(app); // good idea in order unify all init operations
+        // (db connection, data init, app init and so on) to work with promises
 };
 
 module.exports = {
---
+++ node-js/Lectures/project-structure/app/controllers/all.controller.js
@@ -0,0 +1,42 @@
+const validation = (req, res, next) => {
+    const item = req.body;
+
+    if (typeof item !== undefined &&
+        typeof item.text === 'string' &&
+        item.text.length > 3) {
+        return next();
+    }
+
+    req.flash('error', 'Invalid model');
+
+    return res.redirect('/form');
+};
+
+const getItems = (req, res, data) => {
+    return data.items.getAll()
+        .then((items) => {
+            console.log(items[0].id);
+            return res.render('items/all', { context: items });
+        });
+};
+
+const submitItem = (req, res, data) => {
+    const item = req.body;
+
+    data.items.create(item)
+        .then((dbItem) => {
+            console.log(dbItem);
+            console.log(dbItem.id);
+            console.log(typeof dbItem.id);
+            return res.redirect('/items/' + dbItem.id);
+        })
+        .catch((err) => {
+            res.send('Sorry bro');
+        });
+};
+
+module.exports = {
+    validation,
+    getItems,
+    submitItem,
+};
