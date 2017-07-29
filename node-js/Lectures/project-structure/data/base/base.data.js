b99d7e1cb844c2338a906ce61c888e7cdf2d0b3c
flash <k.kostov.eu@gmail.com>
2017-07-29 23:28:55 +0300

    testing traning

--- node-js/Lectures/project-structure/data/base/base.data.js
+++ node-js/Lectures/project-structure/data/base/base.data.js
@@ -7,46 +7,41 @@ class BaseData {
         this.collection = this.db.collection(this.collectionName);
     }
 
-    filterBy(props) {
-        return this.collection.find(props)
-            .toArray();
-    }
+    // filterBy(props) {
+    //     return this.collection.find(props)
+    //         .toArray();
+    // }
 
     getAll() {
         const result = this.collection.find({})
             .toArray();
-
-        if (this.modelClass.toViewModel) {
-            result.then((models) => {
+        if (this.modelClass.createInstance) {
+            return result.then((models) => {
                 return models.map((model) => {
                     return this.modelClass.createInstance(model);
                 });
             });
+        } else {
+            return result;
         }
-
-        return result;
     }
 
-    create(data) {
-        // const model = this.modelClass.createInstance(data);
+    // create(data) {
+    //     // const model = this.modelClass.createInstance(data);
 
-        // if (this._isModelValid(model)) {
-        //     return this.collection.insert(model)
-        //         .then((dbModel) => {
-        //             return this.modelClass.createInstance(dbModel.ops[0]);
-        //         });
-        // }
-        // return Promise.reject('Not a Valid Model');
+    //     // if (this._isModelValid(model)) {
+    //     //     return this.collection.insert(model)
+    //     //         .then((dbModel) => {
+    //     //             return this.modelClass.createInstance(dbModel.ops[0]);
+    //     //         });
+    //     // }
+    //     // return Promise.reject('Not a Valid Model');
 
 
-    }
+    // }
 
     _getCollectionName() {
         return this.modelClass.name.toLowerCase() + 's';
-    }
-
-    _isModelValid() {
-        return false;
     }
 }
 
--- node-js/Lectures/project-structure/test/unit/data.tests.js
+++ node-js/Lectures/project-structure/test/unit/data.tests.js
@@ -1,51 +1,88 @@
-const { expect } = require('chai');
+const expect = require('chai').expect;
 const { BaseData } = require('../../data/base/base.data');
 const sinon = require('sinon');
 
 describe('Base data get all', () => {
-    const db = {
-        collection: () => { },
+    const ModelClass = class {
     };
-    let ModelClass = null;
-    let validator = null;
-    let data = null;
-    let items = [];
-
-    const toArray = () => {
-        return Promise.resolve(items);
+    const validator = {
+        isValid: () => true,
     };
 
-    const find = () => {
-        return {
-            toArray,
-        };
-    };
+    let sut = null;
+    const items = [1, 2, 3, 4];
+    let db = null;
 
-    beforeEach(() => {
-        items = [1, 2, 3, 4];
-        validator = {
-            isValid: () => true,
+    before(() => {
+        db = {
+            collection: sinon.stub().returns({
+                find: sinon.stub().returns({
+                    toArray: sinon.stub().returns(Promise.resolve(items)),
+                }),
+            }),
         };
-
-        ModelClass = class {
-        };
-
-        sinon.stub(db, 'collection')
-            .callsFake(() => {
-                return {
-                    find,
-                };
-            });
-        // arrange
-        data = new BaseData(db, ModelClass, validator);
     });
 
-    it('expenct to return items, when items in db', () => {
+    it('expect to return items, when items in db', () => {
+        sut = new BaseData(db, ModelClass, validator);
         // act
-        return data.getAll()
+        return sut.getAll()
             .then((models) => {
                 expect(models).to.deep.equal(items);
+                expect(db.collection().find.callCount).to.equal(1);
+                expect(db.collection().find().toArray.callCount).to.equal(1);
+                expect(db.collection().find.calledWith({})).to.equal(true);
             });
-        // assert
     });
+
+    describe('WITH MODELCLASS SET PROPER MOCK', () => {
+        let mySut = null;
+        let modelStub = null;
+        before(() => {
+            modelStub = {
+                name: {
+                    toLowerCase: () => {
+                        return 'model';
+                    },
+                },
+                createInstance: sinon.stub().returns(true),
+            };
+
+            mySut = new BaseData(db, modelStub, validator);
+        });
+        it('expect to return items, when items in db AND VIEW MODEL', () => {
+            // act
+            return mySut.getAll()
+                .then((models) => {
+                    expect(modelStub.createInstance.callCount).to.equal(4);
+                    expect(modelStub.createInstance.calledWith(1)).to.equal(true);
+                    expect(modelStub.createInstance.calledWith(2)).to.equal(true);
+                    expect(modelStub.createInstance.calledWith(3)).to.equal(true);
+                    expect(modelStub.createInstance.calledWith(4)).to.equal(true);
+                });
+            // assert
+        });
+    });
+
+    // describe('WITH MODELCLASS SET', () => {
+    //     beforeEach(() => {
+    //         ModelClass = class {
+    //             static createInstance(model) {
+    //                 return model + '1';
+    //             }
+    //         };
+
+    //         sut = new BaseData(db, ModelClass, validator);
+    //     });
+    //     it('expect to return items, when items in db AND VIEW MODEL', () => {
+    //         // act
+    //         return sut.getAll()
+    //             .then((models) => {
+    //                 models.forEach((model, index) => {
+    //                     expect(model).to.deep.equal(items[index] + '1');
+    //                 });
+    //             });
+    //         // assert
+    //     });
+    // });
 });
