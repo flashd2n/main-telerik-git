57350fa54f50c60bf6e0621a9030c1cebb6ef4af
flash <k.kostov.eu@gmail.com>
2017-10-01 21:37:40 +0300

    scrap workstation

---
+++ asp-net-mvc/training/lectures/mvc-essentials/PlayAround/PlayAround/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlayAround.Startup))]
+namespace PlayAround
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
